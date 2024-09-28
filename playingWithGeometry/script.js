b073f9ee91837c4d1116f5cb03f6cc52db6bf56d
Juttacci <guy.hazoume@epitech.eu>
2024-09-29 00:39:50 +0100

    fix: Imporve the resizing, feat: add orbit controls

--- playingWithGeometry/script.js
+++ playingWithGeometry/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector('canvas.webgl');
 
@@ -15,14 +16,31 @@ scene.add(box)
 
 //Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
+//Resize Event
+window.addEventListener('resize', () => {
+
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
 //Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+//Orbit Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 
 //Rendering
@@ -31,4 +49,20 @@ const renderer = new THREE.WebGLRenderer(
     canvas : canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+//Animation
+const tick = () => {
+
+    //Update controls
+    controls.update()
+
+    //Render
+    renderer.render(scene, camera)
+
+    //Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
