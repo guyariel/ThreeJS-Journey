81557c44e8fee1c3c1e74673a36e6a371066275b
Juttacci <guy.hazoume@epitech.eu>
2024-09-29 13:49:02 +0100

    ci: Create the geometry group and add a box and a cone

--- playingWithGeometry/script.js
+++ playingWithGeometry/script.js
@@ -6,13 +6,39 @@ const canvas = document.querySelector('canvas.webgl');
 //Scene
 const scene = new THREE.Scene()
 
-//Objects
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const boxMaterial = new THREE.MeshBasicMaterial({color : 'blue'})
-const box = new THREE.Mesh(boxGeometry, boxMaterial)
+//Axes Helpers
+const axesHelpers = new THREE.AxesHelper();
+//scene.add(axesHelpers)
+
+/**
+ * Objects
+ */
+const numGeometries = 8
+const radius = 5
+
+const geometryGroup = new THREE.Group()
+scene.add(geometryGroup)
+
+//Box
+const box = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 'red'})
+)
+box.position.set(Math.cos((1 / numGeometries) * Math.PI * 2) * radius, 0,
+ Math.sin((1 / numGeometries) * Math.PI * 2) * radius)
+
+//Cone
+const cone = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 1, 3),
+    new THREE.MeshBasicMaterial({color : 'blue'})
+)
+cone.position.set(Math.cos((2 / numGeometries) * Math.PI * 2) * radius, 0,
+ Math.sin((2 / numGeometries) * Math.PI * 2) * radius)
 
 
-scene.add(box)
+
+geometryGroup.add(box, cone)
+
 
 //Sizes
 const sizes = {
@@ -33,8 +59,40 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+//Fullscreen renderer
+window.addEventListener('dblclick', () => {
+
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullscreenElement)
+    {
+        //if the web browser supports requestFullscreen
+        if (canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }
+        // if not let's check webkitRequestFullscreen
+        else if (canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else 
+    {
+        //Same for exit fullscreen
+        if (document.exitFullscreen)
+        {
+            document.exitFullscreen()
+        }
+        else if (document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
 //Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
 camera.position.z = 3
 scene.add(camera)
 
